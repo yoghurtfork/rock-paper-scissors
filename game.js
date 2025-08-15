2b93172562d442af3856de58c772e8cba3ef31b7
yoghurtfork <limyt176@gmail.com>
2025-08-16 01:18:21 +0800

    Add playRound function to game.js

--- game.js
+++ game.js
@@ -16,6 +16,53 @@ function getHumanChoice(){
     return choice;
 }
 
+function playRound(humanChoice, computerChoice){
+    let str = "";
+    if (humanChoice == "rock"){
+        if (computerChoice == "rock"){
+            str = "Tie!";
+        }
+        else if (computerChoice == "paper"){
+            str = "You lose! Paper beats rock.";
+            computerScore++;
+        }
+        else {
+            str = "You win! Rock beats scissors.";
+            humanScore++;
+        }
+    }
+    else if (humanChoice == "paper"){
+        if (computerChoice == "paper"){
+            str = "Tie!";
+        }
+        else if (computerChoice == "rock"){
+            str = "You win! Paper beats rock.";
+            humanScore++;
+        }
+        else {
+            str = "You lose! Scissors beats paper.";
+            computerScore++;
+        }
+    }
+    else {
+        if (computerChoice == "scissors"){
+            str = "Tie!";
+        }
+        else if (computerChoice == "rock"){
+            str = "You lose! Rock beats scissors.";
+            computerScore++;
+        }
+        else {
+            str = "You win! Scissors beats paper.";
+            humanScore++;
+        }
+    }
+    console.log(str);
+}
+
 let humanScore = 0;
 let computerScore = 0;
 
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
