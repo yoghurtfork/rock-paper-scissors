30efee7cc164d0f081015a52a532727c9d4266ea
yoghurtfork <limyt176@gmail.com>
2025-08-16 00:52:51 +0800

    Add getHumanChoice function to game.js

--- game.js
+++ game.js
@@ -1,10 +1,19 @@
 function getComputerChoice(){
-    let n = Math.floor(Math.random()*3);
-    switch(n){
+    let choice = Math.floor(Math.random()*3);
+    switch(choice){
         case 0: return "rock";
         case 1: return "paper";
         case 2: return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let choice = "choice";
+    while (choice != "rock" && choice != "paper" && choice != "scissors"){
+        choice = prompt("Choose 'rock', 'paper', or 'scissors'!", "choice");
+        choice = choice.toLowerCase();
+    }
+    return choice;
+}
+
+console.log(getHumanChoice());
